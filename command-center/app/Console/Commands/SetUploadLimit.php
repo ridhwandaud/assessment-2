d5f38a716ead2d4acc5d827e31e58acdbc7721fe
ridhwan <ridhwan@jeweldv.com>
2022-01-25 23:27:50 +0800

    Commant laravel agent api

---
+++ command-center/app/Console/Commands/SetUploadLimit.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use GuzzleHttp\Client;
+
+class SetUploadLimit extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'helper:setupload {limit}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Pass upload limit to agent';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $limit = $this->argument('limit');
+        $response = $client = new Client([
+            // Base URI is used with relative requests
+            'base_uri' => 'http://localhost:8080/upload-limit/' . $limit,
+            // You can set any number of default request options.
+            'timeout'  => 2.0,
+        ]);
+
+        $response = $response->getBody()->getContents();
+
+        print_r($response);
+    }
+}
